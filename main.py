b6dbcbb9f5429e715a8cc8172bbcbdaa8841e1cd
Sanjhanaa Ananthraman <sanjhanaaananth2006@gmail.com>
2026-04-07 17:01:16 +0530

    Add model and set up to take user prompts

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+client=genai.Client(api_key=api_key)
+response = client.models.generate_content(model='gemini-2.5-flash', contents=messages) #This is a GenerateContentResponse object
+if response.usage_metadata:
+    print(response.text)
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+else:
+    raise RuntimeError("Response was not generated successfully.")
+
+def main():
+    print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
